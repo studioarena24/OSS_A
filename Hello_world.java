a7164d598a79a2eb4df4a8898dff2fc1796dc3e6
wlsg159 <43737872+wlsg159@users.noreply.github.com>
2018-10-31 14:17:57 +0900

    Add files via upload

---
+++ Hello_world.java
@@ -0,0 +1,12 @@
+package test01;
+
+public class Hello_world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+	}
+
+}
